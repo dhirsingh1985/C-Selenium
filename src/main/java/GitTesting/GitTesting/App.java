44190ca8708c25af5e7f54c6f0b7ee2f695ead06
dhirsingh1985 <59521067+dhirsingh1985@users.noreply.github.com>
2021-03-21 16:12:10 +1000

    Local repo created and then pushed to Git

---
+++ src/main/java/GitTesting/GitTesting/App.java
@@ -0,0 +1,18 @@
+package GitTesting.GitTesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        	
+    }
+}
